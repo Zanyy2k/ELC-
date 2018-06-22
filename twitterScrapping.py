9635cb0275772cdbbee50aa4a6230fe8b5134467
Zanyy2k <yuzongdongsg@hotmail.com>
2018-06-22 18:02:21 +0800

    Twitter Scrapping
    Using python and Tweepy to get the follower information

---
+++ twitterScrapping.py
@@ -0,0 +1,49 @@
+import time
+import csv
+import sys
+import tweepy
+from tweepy.auth import OAuthHandler
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+consumer_key = 'BaB4WM1AxMqxYws1zV4abyNZU'
+consumer_secret = 'sVEIgNUhJePpfzzoUHpw4BfN81tm3DuUaMNDME9mGmfCnrNne4'
+access_token = '780304879227052032-JAtPkXPwZcksCXjghnPrplYdUdorExr'
+access_secret = '97ssbB6CDC5zjDFaAPIhsQPm4ZYZPLnuq7gyrmAnKEly0'
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token,access_secret)
+api = tweepy.API(auth)
+
+# In this example, the handler is time.sleep(15 * 60)
+def limit_handled(cursor):
+    while True:
+        try:
+            yield cursor.next()
+        except tweepy.RateLimitError:
+            print('System sleeping')
+            time.sleep(15 * 60)
+            continue
+        except tweepy.TweepError:
+            print('System sleeping')
+            time.sleep(15 * 60)
+        except StopIteration:
+            break
+
+# Open a new csv file with title row            
+f = csv.writer(open('followers_JML.csv', 'wb'))
+f.writerow(["ScreenName", "Name", "Location","Followers"])
+
+# followers = tweepy.Cursor(api.followers,id="JoMaloneLondon").items()
+# for follower in followers:
+for follower in limit_handled(tweepy.Cursor(api.followers, id="JoMaloneLondon").items()):
+    if follower.friends_count > 300:
+        screenName = follower.screen_name.encode("utf-8")
+        name = follower.name.encode("utf-8")
+        location = follower.location.encode("utf-8")
+        fcounts = follower.followers_count
+        print(screenName,name,location,fcounts) 
+        f.writerow([screenName,name,location,fcounts])
+f.close()    
+
