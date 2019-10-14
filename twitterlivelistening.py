6dee8a00400bf32cead4477d1fc1c85fb443b431
ZongDong <yuzongdongsg@hotmail.com>
2019-10-14 20:34:15 +0800

    Update twitterlivelistening.py

--- twitterlivelistening.py
+++ twitterlivelistening.py
@@ -1,34 +1,35 @@
 import tweepy
 from tweepy import Stream
-from tweepy.streaming import StreamListener 
+from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
-import json
+
 consumer_key = ''
 consumer_secret = ''
 access_token = ''
 access_secret = ''
- 
+
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
 
+
 class MyListener(StreamListener):
- 
+
     def on_data(self, data):
         try:
-            print data
+            print (data)
             with open('python.json', 'a') as f:
                 f.write(data)
                 return True
         except BaseException as e:
             print("Error on_data: %s" % str(e))
             time.sleep(5)
-            print 'System sleeping 5 minutes'
+            print("System sleeping 5 minutes")
             return True
+
         def on_error(self, status):
             print(status)
             return True
-print "I am here1"
+
 twitter_stream = Stream(auth, MyListener())
-print "I am here2"
-twitter_stream.filter(track=['#russia2018'])
+twitter_stream.filter(track=['#nba'])
